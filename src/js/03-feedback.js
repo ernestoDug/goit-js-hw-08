be3ad8a8187a7cbcc6aeb65c3d31d4edb77782d7
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-06-04 02:42:38 +0300

    3v

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,52 @@
 import throttle  from 'lodash.throttle';
 
-// - отримую посилання на форму 
+//  посилання на форму 
 const form = document.querySelector(".feedback-form");
-// - отримую посилання на поле відгука
+//  посилання на поле відгука
 const formTextArea = document.querySelector(".feedback-form textarea");
-// - отримую посилання на кнопку 
+//  посилання на ІНПУТ ИМЕЙЛА  
 const formInput = document.querySelector(".feedback-form input");
-// - чипляю слухачів
+// СЛУХАЧ ФОРМИ
 form.addEventListener("submit", submitWork );
+// СЛУХАЧ ТЕКСТ АРЕА
 formTextArea.addEventListener("input", throttle(inputWork, 500));
+// СЛУХАЧ ІНПУТА ПОШТИ
 formInput.addEventListener("input", throttle(inputWork, 500));
-
 // ОБ'ЄКТ ДЛЯ ЗБЕРІГАННЯ 
 const feedbackObj = {};
-
-// виклик функції перевірки даних в локал сторидж 
+// виклик функції перевірки СХОВИЩА 
 getFromStor();
 
-
-// - прописую обробника для форми
+// ОБРОБНИК Форми
 function submitWork (event) {
 // забороняю поведінку за замовчуванням 
 event.preventDefault();
+// ВИВОДЖУ ОБ'ЄКТ В КОНСОЛЬ 
+console.log(feedbackObj);
 // ОЧИШУЮ ПОЛЯ ФОРМИ;
 event.target.reset();
-// очищую локал сторидж 
+// очищую СХОВИЩЕ
 localStorage.removeItem("feedback-form-state")
 }
 
-// прописую обробника для текстового поля відгука
+// ОБРОБНИК ІНПУТА
 function inputWork (event) {
 // зберігаю данні введнеі в текстове поле 
 feedbackObj[ event.target.name ] = event.target.value;
-const feedbackObjJASON = JSON.stringify(feedbackObj);
-// додаю до сховища отриманий текст
-localStorage.setItem("feedback-form-state", feedbackObjJASON);
+// НАПОВНЮЮ СХОВИЩЕ ОБ'ЄКТОМ
+localStorage.setItem("feedback-form-state",  JSON.stringify(feedbackObj));
 }
-
-// - функція перевірки  даних в локал сторидж 
+// ПЕРЕВІРКА ДАННИХ СХОВИЩА
 function getFromStor() {
-    const savedInlocalSt = localStorage.getItem("feedback-form-state");
+        const savedInlocalSt = localStorage.getItem("feedback-form-state");
+        const savedInlocalStParse = JSON.parse(savedInlocalSt);
+        // УМОВА ЯКЩО СХОВИЩЕ НЕ ПОРОЖНЄ 
     if (savedInlocalSt)
     {
-    //  оновлення Dom 
-    formTextArea.value = savedInlocalSt;
+        //  оновлення В Dom  ТЕКСТ АРЕА
+        formTextArea.value = savedInlocalStParse.message;
+                //  оновлення В Dom  ТЕКСТ ІНПУТА ІМЕЙЛА
+        formInput.value = savedInlocalStParse.email;
     }
 }
 
